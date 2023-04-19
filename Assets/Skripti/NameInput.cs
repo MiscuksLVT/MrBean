0032c2f78f7e792e144d49dea8207977638b3432
Daniils <daniils.miscuks@gmail.com>
2023-04-19 12:22:06 +0300

    Sagatavots viss pirmas skripta rakstisana

---
+++ Assets/Skripti/NameInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameInput : MonoBehaviour {
+	public int kadruSk = 0;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Uzsaka programma! Tu esi "+kadruSk+" kadra");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Kadrs: "+kadruSk);
+		kadruSk++;
+	}
+}
