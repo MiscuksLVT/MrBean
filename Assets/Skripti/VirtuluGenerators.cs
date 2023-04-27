bea1191b5f4bdcf857a6a9d3f7cc4a67842ecdaf
Daniils <daniils.miscuks@gmail.com>
2023-04-27 11:19:08 +0300

    Projekts ir pabeigts!

---
+++ Assets/Skripti/VirtuluGenerators.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtuluGenerators : MonoBehaviour
+{
+	public GameObject[] virtuluSagataves;
+	public float laikaintervals = 1f;
+	float minX, maxX;
+	private Transform krasnsTranform;
+
+
+	// Use this for initialization
+	void Start()
+	{
+        krasnsTranform = GetComponent<Transform>();
+
+	}
+	public void SagatavotVirtulus(bool stavoklis)
+	{
+		if (stavoklis)
+			StartCoroutine(Generet());
+		else
+			StopAllCoroutines();
+
+		
+	}
+	IEnumerator Generet()
+	{
+		while (true)
+		{
+			minX = krasnsTranform.position.x - 15;
+			maxX = krasnsTranform.position.x +15;
+			var cipars = Random.Range(minX, maxX);
+			var pozicija =new Vector2(cipars,transform.position.y);
+			Instantiate(virtuluSagataves[Random.Range(0, virtuluSagataves.Length)], pozicija, Quaternion.identity, krasnsTranform);
+            yield return new WaitForSeconds(laikaintervals);
+		}
+	}
+
+
+
+}
